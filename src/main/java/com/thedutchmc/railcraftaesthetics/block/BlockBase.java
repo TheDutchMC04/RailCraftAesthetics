346d2586a9d42a36f18865fbcfad9d7ee17fb3a8
TheDutchMC04 <tobias64a@gmail.com>
2018-06-20 16:14:09 +0200

    blockbase

--- src/main/java/com/thedutchmc/railcraftaesthetics/block/BlockBase.java
+++ src/main/java/com/thedutchmc/railcraftaesthetics/block/BlockBase.java
@@ -1,5 +1,30 @@
 package com.thedutchmc.railcraftaesthetics.block;
 
-public class BlockBase {
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 
+import com.thedutchmc.railcraftaesthetics.proxy.*;
+
+class BlockBase extends Block {
+
+    String name;
+
+    BlockBase(Material material, String name) {
+        super(material);
+
+        this.name = name;
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+
+    public void registerItemModel(Item item) {
+        proxy.registerItemRenderer(item, 0, name);
+    }
+
+    public Item createItemBlock() {
+        return new ItemBlock(this).setRegistryName(getRegistryName());
+    }
 }
